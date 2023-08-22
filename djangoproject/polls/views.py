8a3b67f8d804bdf06fba1e93cec6b255bc121c22
admin <023>
2023-08-22 19:08:35 +0300

    improve view detail

--- djangoproject/polls/views.py
+++ djangoproject/polls/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
 
 # Create your views here.
 # =====================================================================================================================
 from .models import Question
-from django.template import loader
 
 
 def index(request):
@@ -18,16 +20,36 @@ def index(request):
     # return HttpResponse(output)
 
     # 3=TEMPLATE --------------------------------------
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html")
+    # context = {
+    #     "latest_question_list": latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    # 4=shortcut --------------------------------------
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # 1=simple plane static text --------------------------------------
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+    # 2=ORM work --------------------------------------
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, "polls/detail.html", {"question": question})
+
+    # 3=SHORTCUT --------------------------------------
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+    # 4=
+
 
 
 def results(request, question_id):
